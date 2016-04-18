146c62dd8038b237da5dfd3781ca10da303a068f
Dyson <linshihting1029@gmail.com>
2016-04-18 17:24:33 +0800

    add icon

--- MyScoreBoardapp/LoginPage.swift
+++ MyScoreBoardapp/LoginPage.swift
@@ -12,7 +12,7 @@ class LoginPage: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
