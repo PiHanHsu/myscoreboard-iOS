3d0cc047c37027f712c67a603060affefb339c19
Dyson <linshihting1029@gmail.com>
2016-04-18 17:22:46 +0800

    Add navigationBar backGroundColor

--- MyScoreBoardapp/AppDelegate.swift
+++ MyScoreBoardapp/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().barTintColor = UIColor(red: 82.0/255.0, green: 142.0/255.0, blue: 233.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.redColor()
         return true
     }
 
