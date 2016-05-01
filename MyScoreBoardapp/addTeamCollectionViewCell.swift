c173dfdb8cc2e8258e961926f4e4920d0b4d9091
stephyang <stephanie1635945@gmail.com>
2016-05-01 11:51:30 +0800

    add  addTeamCollectionViewCell

---
+++ MyScoreBoardapp/addTeamCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  addTeamCollectionViewCell.swift
+//  MyScoreBoardapp
+//
+//  Created by stephanie yang on 2016/5/1.
+//  Copyright © 2016年 PiHan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class addTeamCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var addTeamImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
