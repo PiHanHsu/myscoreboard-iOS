03b97b1f90cd3d404fbfbcc25f0e8f7c4661afd5
stephyang <stephanie1635945@gmail.com>
2016-05-03 01:05:49 +0800

    Add Teams with Singleton

--- MyScoreBoardapp/Teams.swift
+++ MyScoreBoardapp/Teams.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+
+class Teams{
+    var teams = [Team]()
+   
+    static let sharedInstance = Teams()
+    
+    private init() {}
+    
+    
+    func addTeam(team:Team)  {
+        teams.append(team)
+    }
+    
+    func removeAllTeams() {
+        teams.removeAll()
+    }
+    
+    func updateTeam(updatedTeam:Team) {
+        
+        for team in teams {
+            
+            if (team.TeamId! == updatedTeam.TeamId!) {
+                team.players = updatedTeam.players
+            }
+        }
+    }
+    
+    func deleteTeam(teamToDelete:Team) {
+        
+        var position = -1;
+        
+        for (index,team) in teams.enumerate() {
+            
+            if (team.TeamId! == teamToDelete.TeamId!) {
+                position = index
+                break
+            }
+        }
+        
+        if position != -1 {
+            teams.removeAtIndex(position)
+        }
+        
+    }
+    
+}
+
