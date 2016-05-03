4f7c3ef0f2ebbcee061f8e528eb4c65fe714386e
stephyang <stephanie1635945@gmail.com>
2016-05-03 22:12:09 +0800

    add gameTime picker

--- MyScoreBoardapp/AddTeamTableViewController.swift
+++ MyScoreBoardapp/AddTeamTableViewController.swift
@@ -8,9 +8,24 @@
 
 import UIKit
 
-class AddTeamTableViewController: UITableViewController,AddMemberDelegate {
+class AddTeamTableViewController: UITableViewController,AddMemberDelegate,UIPickerViewDelegate,UIPickerViewDataSource {
 
     @IBOutlet var pickerBackgroundView: UIView!
+    @IBOutlet weak var gameTimepicker: UIPickerView!
+    
+    let dayArray = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+    let startTimeArray = ["00:00","00:30","01:00:","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","06:30","07:00:","07:30","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00:","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00:","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30"]
+    let endTimeArray = ["00:00","00:30","01:00:","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","06:30","07:00:","07:30","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00:","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00:","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30"]
+   
+    
+    var daytime:String?
+    var starttime:String?
+    var endtime:String?
+    
+    var totalTime:String?
+    //will add
+    var gameTime:AddTeamLabelTableViewCell?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +33,14 @@ class AddTeamTableViewController: UITableViewController,AddMemberDelegate {
         let teams = Teams.sharedInstance
         
         print(teams)
+        
+        daytime = dayArray[0]
+        starttime = startTimeArray[0]
+        endtime = endTimeArray[0]
+        
+        
+        
+
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -84,6 +107,9 @@ class AddTeamTableViewController: UITableViewController,AddMemberDelegate {
             cell.addTeamDetailIcon.image = UIImage(named:"t_user" )
             cell.addTeamDetailText.placeholder = "聚會時間"
             cell.addTeamDetailText.userInteractionEnabled = false
+            cell.addTeamDetailText.text = ""
+            gameTime = cell
+            
             
             return cell
             
@@ -114,13 +140,16 @@ class AddTeamTableViewController: UITableViewController,AddMemberDelegate {
             return cell
         }
     }
+    
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
         //Add cases here to move to different segues
       
         if indexPath.section == 1{
+            
+            self.pickerBackgroundView.frame = CGRect(x: 0, y: UIScreen.mainScreen().bounds.height/3*2 , width: UIScreen.mainScreen().bounds.width , height: UIScreen.mainScreen().bounds.height/3 )
             self.view.addSubview(pickerBackgroundView)
             
-      
         }
         
         if indexPath.section == 2{
@@ -131,8 +160,71 @@ class AddTeamTableViewController: UITableViewController,AddMemberDelegate {
         
         }
         }
+   
     
-//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //首先先把要顯示的資料分別存在兩個Array
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3   //有多少個 component
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //設定每個 component 有幾列
+        if component == 0{
+            return dayArray.count //第一個Component要顯示的數量
+        }else if component == 1{
+            return startTimeArray.count //第二個Component要顯示的數量
+            
+        }else{
+            return endTimeArray.count  //第三個Component要顯示的數量
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0{
+            return dayArray[row]  //第一個Component要顯示的文字
+            
+        }else if component == 1{
+            return startTimeArray[row] //第二個Component要顯示的數量
+            
+        }else{
+            return endTimeArray[row]   //第三個Component要顯示的文字
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if component == 0{
+            daytime = dayArray[row]
+            //            print("number: \(dayArray[row])") //點擊第一個Component
+        }else if component == 1 {
+            
+            starttime = startTimeArray[row]
+            //            print("startTime: \(startTimeArray[row])")   //點擊第二個Component
+        }else{
+            endtime = endTimeArray[row]
+            //            print("endTime: \(endTimeArray[row])")   //點擊第三個Component
+        }
+        
+        totalTime = daytime!+" "+starttime!+"-"+endtime!
+        //        pickerView.removeFromSuperview()
+//        (totalTime!)
+    }
+    
+    @IBAction func finishAddTimeBtn(sender: AnyObject) {
+        
+        
+            gameTime?.addTeamDetailText.text = totalTime!
+            pickerBackgroundView.removeFromSuperview()
+    
+    }
+    
+    
+    
+    
+  
+
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 //        if segue.identifier == "showAddGameLocation" {
 //            let desViewcontroller = segue.destinationViewController as! AddTeamMemberViewController
 ////            desViewcontroller.student = class1.students[0]
