cb2d412945e90d4840a3852e3feb75be91ecdbf0
Dyson <linshihting1029@gmail.com>
2016-05-04 11:12:04 +0800

    add xib OrTableViewCell

---
+++ MyScoreBoardapp/OrTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  OrTableViewCell.swift
+//  MyScoreBoardapp
+//
+//  Created by MBPrDyson on 5/3/16.
+//  Copyright © 2016 PiHan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class OrTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelInCell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
